365bd1964081e7dd075abd9c3a274b90a3b948a1
Ashish <ashish@biztecno.net>
2022-12-13 15:17:56 +0530

    test

---
+++ src/batch_22_23/Arthematic.java
@@ -0,0 +1,18 @@
+package batch_22_23;
+
+public class Arthematic {
+    public static void main(String[] agr) {
+        int a = 10;
+        int b = a++;
+        // int b=++a;
+        // int b=a--;
+        // int b=--a;
+        System.out.println("-_-_-_-_-_-_START-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println(a == b);
+
+    }
+
+}
---
+++ src/batch_22_23/HelloJava.java
@@ -0,0 +1,12 @@
+package batch_22_23;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello users");
+		String xString="";
+
+	}
+
+}
